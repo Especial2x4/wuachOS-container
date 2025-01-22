ca12a12f6e02a52c518676bb17ddd46d2e559315
Especial2x4 <especial2x4@gmail.com>
2025-01-22 19:22:03 +0000

    agregando atributos activo y fichitas a la clase Player

--- app/Player.py
+++ app/Player.py
@@ -4,6 +4,8 @@ class Player:
         self.user_id = user_id
         self.username = username
         self.first_name = first_name
+        self.activo = True
+        self.fichitas = 6
 
     def get_name(self):
         return self.username if self.username else self.first_name
