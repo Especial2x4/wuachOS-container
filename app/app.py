fb10917708d921549774753fb209ba2f48f6d7d5
Especial2x4 <especial2x4@gmail.com>
2025-01-27 19:36:07 +0000

    implementando funcionalidad de ceder

--- app/app.py
+++ app/app.py
@@ -112,6 +112,10 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await listar_para_extraer(update, context)
     elif query.data.startswith('extraer_player'):
         await extraer_player(update, context)
+    elif query.data.startswith('listar_para_ceder'):
+        await listar_para_ceder(update, context)
+    elif query.data.startswith('ceder_player'):
+        await ceder_player(update, context)
 
 
 
@@ -214,7 +218,7 @@ async def run(update: Update, context: ContextTypes.DEFAULT_TYPE):
             [InlineKeyboardButton("Listar Players", callback_data=f'listar_players_{room_id}')],
             [InlineKeyboardButton("👁️ Espiar", callback_data=f'listar_para_espiar_{room_id}')],
             [InlineKeyboardButton("⚒️ Extraer", callback_data=f'listar_para_extraer_{room_id}')],
-            [InlineKeyboardButton("🤝 Ceder", callback_data=f'ceder_{room_id}')],
+            [InlineKeyboardButton("🤝 Ceder", callback_data=f'listar_para_ceder_{room_id}')],
             [InlineKeyboardButton("🧊 Hibernar", callback_data=f'hibernar_{room_id}')]
         ]
         
@@ -284,6 +288,39 @@ async def extraer_player(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if player and extractor:
         # Mostrar la barra de carga antes de la extracción
         await load_bar(query, context, player, extractor)
+    else:
+        await query.message.reply_text(text="Jugador no encontrado.")
+
+
+async def listar_para_ceder(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    room_id = query.data.split('_')[3]  # Esto varia en base a la callback anterior en este caso viene del teclado de run
+    sala = rooms[room_id]
+    players = sala.get_players()
+    
+    keyboard = [[InlineKeyboardButton(player, callback_data=f'ceder_player_{room_id}_{player}')] for player in players]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    await query.message.reply_text(text="Seleccionar un jugador para ceder una fichita:", reply_markup=reply_markup)
+
+
+async def ceder_player(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    data = query.data.split('_')
+    room_id = data[2]  # Asegurarse de que el índice sea correcto
+    player_name = data[3]
+
+    sala = rooms[room_id]
+    player = next((p for p in sala.players if p.get_name() == player_name), None)
+    user = update.callback_query.from_user
+    cedector = next((p for p in sala.players if p.user_id == user.id), None)
+
+    if player and cedector:
+        # Mostrar la barra de carga antes de la extracción
+        cedector.reducir_fichita()
+        player.aumentar_fichita()
+        await query.message.reply_text(text=f"Has cedido una fichita a {player.get_name()}. Ahora tienes {cedector.get_fichitas()} fichitas.")
+        await context.bot.send_message(chat_id=player.user_id, text=f"{cedector.get_name()} Te ha cedido una fichita. Ahora tienes {player.get_fichitas()} fichitas.")
     else:
         await query.message.reply_text(text="Jugador no encontrado.")
 
